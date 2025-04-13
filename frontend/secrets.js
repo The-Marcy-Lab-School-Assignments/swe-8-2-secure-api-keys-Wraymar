e4a3c32bebd2ab3c9297734042a356a53f8900d5
Wraymar <tyreesewray14@gmail.com>
2025-04-13 19:26:37 -0400

    completed main assignment

---
+++ frontend/secrets.js
@@ -0,0 +1,1 @@
+export const API_KEY = "gILzAK1xjT8YHZDoOl9dUQ2xjQ6oTeLR";
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@
 
 const path = require("path");
 const express = require("express");
+require("dotenv").config();
 
 //////////////////////////
 // Constants
@@ -12,6 +13,7 @@ const express = require("express");
 const port = 8080;
 const pathToDistFolder = path.join(__dirname, "../frontend/dist");
 const app = express();
+const { API_KEY } = process.env;
 
 //////////////////////////
 // Middleware/Controllers
@@ -19,7 +21,21 @@ const app = express();
 
 const serveStatic = express.static(pathToDistFolder);
 
+//we make this asyncronous since we are fetching to the api
+const serveGifs = async (req, res, next) => {
+  try {
+    const url = `https://api.giphy.com/v1/gifs/trending?limit=3&rating=g&api_key=${API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    res.status(503).send(error);
+    //res.send(res.status(503));
+  }
+};
+
 app.use(serveStatic);
+app.get("/api/gifs", serveGifs);
 
 //////////////////////////
 // Listener
